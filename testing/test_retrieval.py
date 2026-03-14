0579995ba8acf984d7350e78dafeb2f143c1a9c9
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2026-03-14 16:32:16 +0530

    approach shift

--- testing/test_retrieval.py
+++
@@ -1,9 +0,0 @@
-from services.summary_service import SummaryService
-
-service = SummaryService()
-
-summary = service.summarize_chapter(
-    metadata={"class": 7, "subject": "science", "chapter": 5}
-)
-
-print(summary)
