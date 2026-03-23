6ab424e32de7f04f7a0ae4ccbf569f111cf43de2
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2026-03-24 00:15:07 +0530

    [fix]: dict of int, str in quiz submit

--- src/models/quiz_schema.py
+++ src/models/quiz_schema.py
@@ -3,7 +3,7 @@ from typing import Dict, Optional
 
 
 class SubmitQuizRequest(BaseModel):
-    student_answers: Dict[str, Optional[str]] = Field(
+    student_answers: Dict[int, Optional[str]] = Field(
         ...,
         description="Student answers as dict: {'1': 'A', '2': 'B', ...}. Use null for unanswered questions.",
     )
